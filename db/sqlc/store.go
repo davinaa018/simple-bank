6d40a048e707216e262941f43ecd09887a677ccf
Diego Avina <davinaa018@gmail.com>
2024-12-28 19:25:59 -0700

    Added New Tests and Fixed Deadlock Error

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -53,6 +53,7 @@ type TransferTxResult struct {
 	ToEntry       Entry    `json:"to_entry"`
 }
 
+
 // TransferTx performs a mopney transfer from one account to the other
 // it creates a transfer record, add account entries, and update accounts' balance within a single database transaction
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error){
@@ -60,6 +61,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
+
 
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
 			FromAccountID: arg.FromAccountID,
@@ -70,7 +72,7 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		if err != nil {
 			return err
 		}
-
+		
 		// Get the account that the transfer is coming from
 		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: arg.FromAccountID,
@@ -92,7 +94,24 @@ func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (Trans
 		}
 
 		// TODO: Update accounts' balance
+		// Get the account that the transfer is coming from, and update its balance
+		result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID: arg.FromAccountID,
+			Amount: -arg.Amount,
+		})
 
+		if err != nil{
+			return err
+		}
+
+		result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+			ID: arg.ToAccountID,
+			Amount: arg.Amount,
+		})
+
+		if err != nil{
+			return err
+		}
 
 
 		return nil
--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,6 +13,7 @@ func TestTransferTx(t *testing.T){
 
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
+	fmt.Println(">> Before:", account1.Balance, account2.Balance)
 
 	// Run n concurrent transfer transactions
 	n := 5
@@ -22,7 +24,8 @@ func TestTransferTx(t *testing.T){
 
 	for i := 0; i < n; i++{
 		go func(){
-			result, err := store.TransferTx(context.Background(), TransferTxParams{
+			ctx := context.Background()
+			result, err := store.TransferTx(ctx, TransferTxParams{
 				FromAccountID: account1.ID,
 				ToAccountID: account2.ID,
 				Amount: amount,
@@ -33,6 +36,7 @@ func TestTransferTx(t *testing.T){
 		}()
 	}
 
+	existed := make(map[int]bool) // To check if the same transfer is executed more than once
 	// Check results
 	for i := 0; i < n; i++{
 		err := <- errs
@@ -73,8 +77,39 @@ func TestTransferTx(t *testing.T){
 
 		_, err = store.GetEntry(context.Background(), toEntry.ID)
 		require.NoError(t, err)
-
+		
 		// TODO: Check accounts
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
 
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		// Check account balance
+		fmt.Println(">> tx:", account1.Balance, account2.Balance)
+		diff1 := account1.Balance - fromAccount.Balance
+		diff2 := toAccount.Balance - account2.Balance
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1 % amount == 0) // diff1 should be a multiple of amount
+
+		k := int(diff1 / amount)
+		require.True(t, k >= 1 && k <= n)
+		require.NotContains(t, existed, k)
+		existed[k] = true
 	}
+
+	// Check the final updated balance
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after:", account1.Balance, account2.Balance)
+	require.Equal(t, account1.Balance - int64(n)*amount, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance + int64(n)*amount, updatedAccount2.Balance)
+
 }
